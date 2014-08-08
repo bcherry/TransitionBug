76397ea11f516460240978f92393fb5653411c39
Ben Cherry <bcherry@gmail.com>
2014-08-08 10:46:23 -0700

    Initial Commit

---
+++ TransitionBug/BCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BCAppDelegate.h
+//  TransitionBug
+//
+//  Created by Ben Cherry on 8/8/14.
+//  Copyright (c) 2014 Ben Cherry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TransitionBug/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TransitionBug
+//
+//  Created by Ben Cherry on 8/8/14.
+//  Copyright (c) 2014 Ben Cherry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BCAppDelegate class]));
+    }
+}
